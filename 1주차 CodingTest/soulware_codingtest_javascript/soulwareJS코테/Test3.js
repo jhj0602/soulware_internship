76ba41095184f7d238013dce55d23fd5f4ad75ae
jhj960918 <jhj960918@gmail.com>
2021-01-09 13:30:37 +0900

    해당 월에 대한 요일 출력

--- 1주차 CodingTest/soulware_codingtest_javascript/soulwareJS코테/Test3.js
+++ 1주차 CodingTest/soulware_codingtest_javascript/soulwareJS코테/Test3.js
@@ -1,6 +1,13 @@
-let name = 'bathingape'
-console.log(name) // bathingape
 
-// let name = 'javascript'
-// console.log(name)
-    // Uncaught SyntaxError: Identifier 'name' has already been declared
+const dayweek = ['일','월','화','수','목','금','토'];
+const moment = require('moment');
+var dayConut = moment().daysInMonth();//이번달 일 수 구하기
+var cnt=0;
+var needDay = dayweek[1];
+for(let i=1;i<=dayConut;i++){
+    if(needDay==dayweek[moment().date(i).day()]){//i에 해당하는 요일 구하기
+        cnt++;
+    }
+    
+}
+console.log(needDay+"요일은 총 "+cnt+"번 있습니다");
