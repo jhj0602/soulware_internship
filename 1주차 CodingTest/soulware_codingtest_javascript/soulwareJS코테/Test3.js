d8f47b6ea186aca738424062d870f9660cc434e6
jhj960918 <jhj960918@gmail.com>
2021-01-11 16:15:43 +0900

    수정

--- 1주차 CodingTest/soulware_codingtest_javascript/soulwareJS코테/Test3.js
+++ 1주차 CodingTest/soulware_codingtest_javascript/soulwareJS코테/Test3.js
@@ -1,9 +1,13 @@
 
+
 const dayweek = ['일','월','화','수','목','금','토'];
 const moment = require('moment');
 var dayConut = moment().daysInMonth();//이번달 일 수 구하기
+console.log(dayConut);
 var cnt=0;
 var needDay = dayweek[1];
+console.log(moment().date(1).day());
+
 for(let i=1;i<=dayConut;i++){
     if(needDay==dayweek[moment().date(i).day()]){//i에 해당하는 요일 구하기
         cnt++;
