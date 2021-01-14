29417a023f7a164f60e4f058f26b0becc7c32b93
jhj960918 <jhj960918@gmail.com>
2021-01-14 19:54:36 +0900

    Axios.get 이미지 업로드

--- 2 주차 Vue.js/VueProject/test2/src/App.vue
+++ 2 주차 Vue.js/VueProject/test2/src/App.vue
@@ -3,7 +3,8 @@
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <button v-on:click="getData">이미지 가져오기</button>
-    <img v-bind:src="imageData"/>
+    <img v-bind:src="imageData" width="200px" />
+   
   </div>
 </template>
 
@@ -27,11 +28,23 @@ export default {
       console.log("getData");
       axios
         .get(
-          'https://images.unsplash.com/photo-1610048899906-d8f64bc45464?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format'
-        ).then((response) => {
-          // this.imageData = Buffer.from(response.data.toString(base64));
-           this.imageData = "data:image/png;base64," + response.data;
-          // console.log(buffer)
+          "https://images.unsplash.com/photo-1610048899906-d8f64bc45464?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format",
+          { responseType: "arraybuffer" }
+        )
+        .then((response) => {
+          // this.imageData = Buffer.from(response.data, "base64");
+          // // this.imageData =  response.data.toString();
+          // console.log(this.imageData);
+          // 참고 https://developpaper.com/question/vue-project-axios-request-graphics-authentication-code-interface-the-interface-returns-the-form-of-file-stream-ask-for-advice-how-to-convert-into-pictures/
+          this.imageData =
+            "data:image/png;base64," +
+            btoa(
+              new Uint8Array(response.data).reduce(
+                (data, byte) => data + String.fromCharCode(byte),
+                ""
+              )
+            );
+          console.log(this.imageData);
         })
         .catch((ex) => {
           console.error(ex);
