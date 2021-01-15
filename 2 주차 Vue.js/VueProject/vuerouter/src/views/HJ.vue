41d5a4aa43885f962d7ada529199032eda2e6e04
jhj960918 <jhj960918@gmail.com>
2021-01-15 17:20:23 +0900

    Vue RouterExample

---
+++ 2 주차 Vue.js/VueProject/vuerouter/src/views/HJ.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hj">
+    <h1>This is an HJ page</h1>
+  </div>
+</template>
+
+
+
+<style>
+        .yellow1 {
+            color: yellow;
+        }
+
+        .black1 {
+            color: black;
+        }
+
+        .box {
+            width: 200px;
+            height: 200px;
+        }
+
+        .timerBox {
+            width: 200px;
+            outline: 2px solid black;
+        }
+
+        .timerBox .time {
+            font-size: 30pt;
+            color: #4C4C4C;
+            text-align: center;
+            font-family: sans-serif;
+        }
+
+        .btnBox {
+            margin: 20px auto;
+            text-align: center;
+        }
+
+        .btnBox .fa {
+            margin: 0px 5px;
+            font-size: 30pt;
+            color: #FAED7D;
+            cursor: pointer;
+        }
+    </style>
