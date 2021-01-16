e73387bbbb0850065f99b89c3cdb128c65d49700
jhj960918 <jhj960918@gmail.com>
2021-01-16 19:15:54 +0900

    vueCLI plugin vuetify

---
+++ 2 주차 Vue.js/VueProject/project1/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ count }}</p>
+    <button @click="count++">추가</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "안녕하세요",
+      count: 1,
+    };
+  },
+};
+</script>
